9dc5fcec6a22a1dc6a1ac4fbbc7f2cb0dfd744bf
Tanguy Bron <tanguy.bron@gmail.com>
2023-01-24 15:37:13 +0100

    connexion to db

--- python/connexionBDD.py
+++ python/connexionBDD.py
@@ -1,12 +1,19 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="azerty",
-    database="MdPManager"
-)
+connection_params = {
+    'host': "localhost",
+    'user': "root",
+    'password': "azerty",
+    'database': "MdPManager",
+}
 
-# faire quelque chose d'utile avec la connexion
+with mysql.connector.connect(**connection_params) as db :
+    with db.cursor(buffered=True) as c:
+        c.execute("SELECT Hash_MdP FROM utilisateurs;")
+        # db.commit()
+        resultats = c.fetchall()
+        print(resultats)
+        # for utilisateur in resultats:
+        #     print(utilisateur)
 
 db.close()
