295a0a05ba2c9d837cc3f14d0d6547d22bdc0237
Tanguy Bron <tanguy.bron@gmail.com>
2023-01-26 18:46:21 +0100

    ajout hash du mdp lors de l'inscription

---
+++ web/scripts/main.js
@@ -0,0 +1,22 @@
+var pass = "mdpsecret";
+console.log("in");
+const button = document.querySelector('.bouton');
+window.onload = function() {
+    const button = document.querySelector('.bouton');
+    button.addEventListener('click', function() {
+        const input_verif = document.querySelector('#verifpassword');
+        const input = document.querySelector('#password');
+        const verifpassword = input_verif.value;
+        const password = input.value;
+        if(password===verifpassword){
+            console.log("mdp égaux");
+            console.log(CryptoJS.SHA256(password).toString());
+            // A faire : 
+            // Enregistrer password dans LocalSession pour pouvoir ensuite l'utiliser comme clef de chiffrement
+        }else{
+            console.log("mdp différents");
+        }
+        //console.log(CryptoJS.SHA256("mdpsecret").toString());
+        // code to execute when button is clicked
+    });
+};
