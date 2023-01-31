da584d0b20012d9e79fefce687fcd4703e467d45
Scar94 <mathis.kermene@hotmail.fr>
2023-01-31 13:33:23 +0100

    profile final

--- web/scripts/popup.js
+++ web/scripts/popup.js
@@ -1,7 +1,31 @@
-function popup(){
-    document.querySelector(".popup").style.display = "flex";
+function PasswordPopup(){
+    document.querySelector("#password-popup").style.display = "flex";
 }
 
-function ClosePopup(){
-    document.querySelector(".popup").style.display = "none";
+function ClosePasswordPopup(){
+    document.querySelector("#password-popup").style.display = "none";
+}
+
+function EmailPopup(){
+    document.querySelector("#email-popup").style.display = "flex";
+}
+
+function CloseEmailPopup(){
+    document.querySelector("#email-popup").style.display = "none";
+}
+
+function UsernamePopup(){
+    document.querySelector("#username-popup").style.display = "flex";
+}
+
+function CloseUsernamePopup(){
+    document.querySelector("#username-popup").style.display = "none";
+}
+
+function DeletePopup(){
+    document.querySelector("#delete-popup").style.display = "flex";
+}
+
+function CloseDeletePopup(){
+    document.querySelector("#delete-popup").style.display = "none";
 }
