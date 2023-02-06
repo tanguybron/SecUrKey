3474118f2f0be67e0fe7c7179572f2dbaaf50135
Tanguy Bron <tanguy.bron@gmail.com>
2023-02-06 12:03:23 +0100

    run grace a reverse proxy nginx (pour futur https)

--- mdpManagerTest/settings.py
+++ mdpManagerTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bod)%9c5&y_3y7h1&@2p88k+7&dqkp&j58qrw#m&z1&egm_#*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','securkey.com']
+ALLOWED_HOSTS = ['localhost','securkey.local']
 
 
 # Application definition
