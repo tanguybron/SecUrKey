955a10abf7d238f9528f41e429fd6fdbeb3182d0
Scar94 <mathis.kermene@hotmail.fr>
2023-02-08 16:05:35 +0100

    account model

--- mdpManagerTest/models.py
+++ mdpManagerTest/models.py
@@ -4,3 +4,12 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+
+class Account(models.Model):
+    title = models.CharField(max_length=50)
+    logo = models.ImageField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+    creation = models.CharField(max_length=50)
+    last_modification = models.CharField(max_length=50)
