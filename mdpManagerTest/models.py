595cca5f1162c3442aedc563a595906bd4e400b0
Scar94 <mathis.kermene@hotmail.fr>
2023-02-08 16:15:15 +0100

    models v2

--- mdpManagerTest/models.py
+++ mdpManagerTest/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+from django.contrib.auth.models import User
 
 class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     logo = models.ImageField()
     username = models.CharField(max_length=50)
