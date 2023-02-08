1ff645f899774fc90179f6e314bc20c07e95a950
Scar94 <mathis.kermene@hotmail.fr>
2023-02-08 11:10:42 +0100

    https marche

--- mdpManagerTest/settings.py
+++ mdpManagerTest/settings.py
@@ -117,6 +117,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://localhost']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
