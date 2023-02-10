92d2656591a77d8eee844b8c4ad1b439b2c8ee4a
Scar94 <118684327+Scar94@users.noreply.github.com>
2023-02-10 16:09:23 +0100

    Delete sql_queries.py

--- mdpManagerTest/sql_queries.py
+++
@@ -1,8 +0,0 @@
-from django.db import connection
-
-def print_users():
-    with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM Utilisateurs;")
-            row = cursor.fetchall()
-
-    print(row)
