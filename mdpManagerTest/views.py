33df23d195575b8720b14b1b9bbdcebc094fc956
Tanguy Bron <tanguy.bron@gmail.com>
2023-02-10 22:37:58 +0100

    note docker-compose

--- mdpManagerTest/views.py
+++ mdpManagerTest/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from . import sql_queries
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
