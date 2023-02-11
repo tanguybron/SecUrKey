2520ece28c6cb99591e79d5766cda583cd17af1a
Tanguy Bron <tanguy.bron@gmail.com>
2023-02-11 17:53:15 +0100

    everything in env file

--- mdpManagerTest/settings.py
+++ mdpManagerTest/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bod)%9c5&y_3y7h1&@2p88k+7&dqkp&j58qrw#m&z1&egm_#*_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
