970c7616c728011598fecd4299c0b67a3502b4bf
Tanguy Bron <tanguy.bron@gmail.com>
2023-02-24 15:17:48 +0100

    extension can make get requests

--- securkey/popup/script.js
+++ securkey/popup/script.js
@@ -1,4 +1,14 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    document.getElementById("url").innerHTML = activeTab.url;
+    document.getElementById("url").innerHTML = "url : " + activeTab.url;
 });
+
+// get source code of the page https://localhost
+const xhr = new XMLHttpRequest();
+const url='https://localhost/passwords';
+xhr.open("GET", url);
+xhr.send();
+xhr.onreadystatechange = (e) => {
+    data = xhr.responseText;
+    console.log(data);
+}
