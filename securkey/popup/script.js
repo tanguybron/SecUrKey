1bb011591e8d6f201c40e30940a1c8973056696c
Tanguy Bron <tanguy.bron@gmail.com>
2023-02-25 14:49:29 +0100

    add buttons copy to clipboard

--- securkey/popup/script.js
+++ securkey/popup/script.js
@@ -1,3 +1,14 @@
+document.getElementById("button_username").addEventListener('click' , function() {
+    navigator.clipboard.writeText(document.getElementById("username").innerHTML.split(" : ")[1]);
+    document.getElementById("button_username").innerHTML = "copied";
+});
+
+document.getElementById("button_password").addEventListener('click' , function() {
+    navigator.clipboard.writeText(document.getElementById("password").innerHTML.split(" : ")[1]);
+    document.getElementById("button_password").innerHTML = "copied";
+});
+
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     active_domain = activeTab.url.replace("https://", "").replace("http://", "").replace("www.", "").replace("/", "");
