d9e563759e8455ef64f476a9fd2d6b8fe212b631
Tanguy Bron <tanguy.bron@gmail.com>
2023-03-07 18:08:35 +0100

    extension with design

--- securkey/scripts/content.js
+++ securkey/scripts/content.js
@@ -1,10 +1,3 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(
-        tabs[0].id,
-        { status: "loaded"},
-    );
-});      
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log("message received");
     if(request.username != undefined){
