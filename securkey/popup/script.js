88074cd73966b6abe3512be50dd130d4820b6487
Tanguy Bron <tanguy.bron@gmail.com>
2023-03-07 12:25:40 +0100

    .

--- securkey/popup/script.js
+++ securkey/popup/script.js
@@ -1,3 +1,35 @@
+function onWindowLoad() {
+    var message = document.querySelector('#message');
+
+    chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+        var activeTab = tabs[0];
+        var activeTabId = activeTab.id;
+
+        return chrome.scripting.executeScript({
+            target: { tabId: activeTabId },
+            // injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
+            func: DOMtoString,
+            // args: ['body']  // you can use this to target what element to get the html for
+        });
+
+    }).then(function (results) {
+        message.innerText = results[0].result;
+    }).catch(function (error) {
+        message.innerText = 'There was an error injecting script : \n' + error.message;
+    });
+}
+
+function DOMtoString(selector) {
+    if (selector) {
+        selector = document.querySelector(selector);
+        if (!selector) return "ERROR: querySelector failed to find node"
+    } else {
+        selector = document.documentElement;
+    }
+    return selector.outerHTML;
+}
+
+
 try{
     document.getElementById("button_username").addEventListener('click' , function() {
         navigator.clipboard.writeText(document.getElementById("username").innerHTML.split(" : ")[1]);
@@ -34,19 +66,23 @@ xhr.onreadystatechange = (e) => {
     }
     try{
         username_found = document.getElementById("username").innerHTML.split(" : ")[1];
-        //password_found = document.getElementById("password").innerHTML.split(" : ")[1];
+        password_found = document.getElementById("password").innerHTML.split(" : ")[1];
         if(username_found == undefined){
             window.location.href = "./not_found.html";
         }
+
+        window.onload = onWindowLoad;
+        
         // else{
         //     var x = document.getElementById("username").value;
         //     if (x == ""){
-        //         document.getElementById("username").value = username_found;
+        //         window.document.getElementById("username").value = username_found;
         //     }
         
         //     var y = document.getElementById("password").value;
         //     if (y == ""){
-        //         document.getElementById("password").value = password_found;
+                
+        //         window.getElementById("password").value = password_found;
         //     }      
         // }
     }catch(err){
