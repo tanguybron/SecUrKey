de8edca236f310f97e2e7925d3276255b8ce183c
Tanguy Bron <tanguy.bron@gmail.com>
2023-03-14 17:10:15 +0100

    extension avec cookies

--- securkey/background.js
+++ securkey/background.js
@@ -10,26 +10,46 @@ function getCookies(domain, name, callback) {
     });
 }
 
-chrome.tabs.onUpdated.addListener(() => {
+function change_state(){
     console.log("clicked");
-    getCookies("https://localhost", "sessionid", function(cookie) {
-        if(cookie == null){
-            chrome.action.setBadgeText({
-                text: "OFF",
+    getCookies("https://localhost", "logged_in_cookie", function(cookie) {
+        if(cookie != null){
+            getCookies("https://localhost", "sessionid", function(cookie) {
+                if(cookie == null){
+                    chrome.action.setBadgeText({
+                        text: "OFF",
+                    });
+                    chrome.action.setBadgeBackgroundColor(
+                        { color: 'red' }
+                    );
+                }else{
+                    chrome.action.setBadgeText({
+                        text: "ON",
+                    });
+                    chrome.action.setBadgeBackgroundColor(
+                        { color: 'green' }
+                    );
+                }
             });
-            chrome.action.setBadgeBackgroundColor(
-                { color: 'red' }
-            );
-        }else{
-            chrome.action.setBadgeText({
-                text: "ON",
-            });
-            chrome.action.setBadgeBackgroundColor(
-                { color: 'green' }
-            );
         }
+        
     });
+}
+
+chrome.tabs.onActivated.addListener(() => {
+    change_state();
 });
+
+chrome.tabs.onUpdated.addListener(() => {
+    chrome.action.setBadgeText({
+      text: "OFF",
+    });
+    chrome.action.setBadgeBackgroundColor(
+       { color: 'red'}
+    )
+    change_state();
+});
+
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
--- securkey/popup/script.js
+++ securkey/popup/script.js
@@ -1,3 +1,56 @@
+function get_pass(active_domain){
+    // get source code of the page https://localhost/passwords_json
+    const xhr = new XMLHttpRequest();
+    console.log("2: "+active_domain);
+    console.log("3: "+active_domain);
+    const url='https://localhost/passwords_json/'+active_domain;
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onreadystatechange = (e) => {
+        data = xhr.responseText;
+        var json;
+
+        json = JSON.parse(data);
+        for(var i = 0; i < json.length; i++) {
+            console.log("4: "+ active_domain);
+            if(json[i].fields.website == active_domain){
+                document.getElementById("username").innerHTML = "username : " + json[i].fields.username;
+                document.getElementById("password").innerHTML = "password : " + json[i].fields.password;
+            }
+        }
+        try{
+            username_found = document.getElementById("username").innerHTML.split(" : ")[1];
+            password_found = document.getElementById("password").innerHTML.split(" : ")[1];
+            if(username_found == undefined){
+                window.location.href = "./not_found.html";
+            }else{
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    chrome.tabs.sendMessage(
+                        tabs[0].id,
+                        { username: username_found, password: password_found },
+                    );
+                });        
+            }
+            
+
+        }catch(err){
+            console.log(err);
+        }
+        
+    }
+
+}
+
+button = document.getElementById("button_login");
+button.addEventListener("click", function() {
+    chrome.tabs.create({'url': "https://localhost/signin"});
+});
+
+// chrome.browserAction.onClicked.addListener(function() {
+//     chrome.tabs.create({'url': "https://localhost/signin"});
+// });
+
+active_domain = "";
 try{
     document.getElementById("button_username").addEventListener('click' , function() {
         navigator.clipboard.writeText(document.getElementById("username").innerHTML.split(" : ")[1]);
@@ -14,58 +67,7 @@ try{
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    active_domain = activeTab.url.replace("https://", "").replace("http://", "").replace("www.", "").split("/")[0];
+    var active_domain = activeTab.url.replace("https://", "").replace("http://", "").replace("www.", "").split("/")[0];
+    console.log("1: " + active_domain);
+    get_pass(active_domain);
 });
-
-
-// get source code of the page https://localhost/passwords_json
-const xhr = new XMLHttpRequest();
-const url='https://localhost/passwords_json';
-xhr.open("GET", url);
-xhr.send();
-xhr.onreadystatechange = (e) => {
-    data = xhr.responseText;
-    var json;
-    // try{
-    //     json = JSON.parse(data);
-    // }catch(err){
-    //     err = err.toString();
-    //     if(err.includes("DOCTYPE")){
-    //         console.log("pas co");
-    //         document.getElementById("connected").style.display = "none";
-    //         document.getElementById("disconnected").style.display = "block";
-    //         chrome.runtime.sendMessage({ status:"OFF" },);
-    //     }else{
-    //         document.getElementById("disconnected").style.display = "none";
-    //         document.getElementById("connected").style.display = "block";
-    //         chrome.runtime.sendMessage({ status:"ON" },);
-    //     }
-    // }
-
-    json = JSON.parse(data);
-    for(var i = 0; i < json.length; i++) {
-        if(json[i].fields.website == active_domain){
-            document.getElementById("username").innerHTML = "username : " + json[i].fields.username;
-            document.getElementById("password").innerHTML = "password : " + json[i].fields.password;
-        }
-    }
-    try{
-        username_found = document.getElementById("username").innerHTML.split(" : ")[1];
-        password_found = document.getElementById("password").innerHTML.split(" : ")[1];
-        if(username_found == undefined){
-            window.location.href = "./not_found.html";
-        }else{
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(
-                    tabs[0].id,
-                    { username: username_found, password: password_found },
-                );
-            });        
-        }
-        
-
-    }catch(err){
-        console.log(err);
-    }
-    
-}
