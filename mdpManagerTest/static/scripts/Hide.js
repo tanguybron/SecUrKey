6a39988ecf818ca758c937a94b79f6167c4b52f8
McBoux <you@example.com>
2023-03-20 19:37:17 +0100

    modif design

--- mdpManagerTest/static/scripts/Hide.js
+++ mdpManagerTest/static/scripts/Hide.js
@@ -2,16 +2,18 @@ const btn3List = document.querySelectorAll('.hide');
 btn3List.forEach(btn3 => {
   const icon = btn3.querySelector('i');
   const password = btn3.closest('.account').querySelector('.password');
-  password.classList.add('hidden'); 
   let passwordShown = false;
+  const passwordLength = password.textContent.length;
+  password.dataset.password = password.textContent;
+  password.textContent = '*'.repeat(passwordLength);
   btn3.addEventListener('click', () => {
     passwordShown = !passwordShown;
     if (passwordShown) {
-      password.classList.remove('hidden');
+      password.textContent = password.dataset.password;
       icon.classList.remove('fa-eye-slash');
       icon.classList.add('fa-eye');
     } else {
-      password.classList.add('hidden');
+      password.textContent = '*'.repeat(passwordLength);
       icon.classList.remove('fa-eye');
       icon.classList.add('fa-eye-slash');
     }
--- mdpManagerTest/static/scripts/btnDelete.js
+++ mdpManagerTest/static/scripts/btnDelete.js
@@ -1,17 +1,20 @@
 function btnDelete() {
-  console.log('button clicked');
-  const btn1 = document.querySelectorAll('.btn1');
-  btn1.addEventListener('click', event => {
-    event.preventDefault();
-    const form = btn1.closest('form');
-    form.parentNode.remove();
-    fetch(form.action, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
-      },
-      body: new URLSearchParams(new FormData(form)).toString()
+  var result = confirm("Are you sure to delete?");
+  if(result){
+    console.log('button clicked');
+    const btn1 = document.querySelectorAll('.btn1');
+    btn1.addEventListener('click', event => {
+      event.preventDefault();
+      const form = btn1.closest('form');
+      form.parentNode.remove();
+      fetch(form.action, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+        },
+        body: new URLSearchParams(new FormData(form)).toString()
+      });
     });
-  });
+  }
 }
